d26f2bfe5030255fef8093898e9b7b4c7e10cbd5
Yahya Nashawati <yahyaisfast@yahoo.com>
2022-09-19 20:09:28 -0700

    UPDATE

--- site.py
+++ site.py
@@ -1,28 +1,28 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
-from urllib import request
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-hostName = "localhost"
-hostPort = 9000
+from io import BytesIO
 
-class MyServer(BaseHTTPRequestHandler):
+
+class handler(BaseHTTPRequestHandler):
+    
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
-        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
-        print(self.request.test)
+        response = BytesIO()
+        response.write(b'This is GET request. ')
 
-myServer = HTTPServer((hostName, hostPort), MyServer)
-print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+    def do_POST(self):
+        content_length = self.headers
+        print(content_length)
+#        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+#        response = BytesIO()
+#        response.write(b'This is POST request. ')
+#        response.write(b'Received: ')
+#        response.write(body)
+#        self.wfile.write(response.getvalue())
 
-try:
-    myServer.serve_forever()
-except KeyboardInterrupt:
-    pass
 
-myServer.server_close()
-print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
+httpd = HTTPServer(('localhost', 8000), handler)
+httpd.serve_forever()
